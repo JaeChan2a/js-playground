8c1a3c0ac1d8eec82dbe8edec37569532326bfe2
kimJaeChan <vpdo1212@gmail.com>
2022-02-24 15:27:43 +0900

    사용자 입력 및 출력 구현 완료 input tag 와 div tag를 사용하여 input 받은 값 계산 구현 완료 ~calculator2

---
+++ calculator/calculator.js
@@ -0,0 +1,15 @@
+//calculator1
+function main(){
+  var out= document.getElementById('output');
+  console.log(out);
+  out.innerHTML=" 계산기 구현하기";
+
+  console.log(10 +3);
+  console.log(10-3);
+  console.log(10 *3);
+  console.log(10/3);
+
+}
+main();
+
+//calculator2
